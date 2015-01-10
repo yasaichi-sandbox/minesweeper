332c8accb48a9a1db8359e75c5e81010dbe8e4f5
yasaichi <yasaichi@users.noreply.github.com>
2015-01-10 11:47:14 +0900

    modify config.rb

--- config.rb
+++ config.rb
@@ -56,10 +56,10 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
@@ -69,4 +69,11 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+end
+
+# Deploy
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+  deploy.branch = 'gh-pages'
 end
